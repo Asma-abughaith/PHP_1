56f6385145108ccd0992ffdabcf28f27d65e995e
Asma-abughaith <115078236+Asma-abughaith@users.noreply.github.com>
2022-10-29 09:29:50 +0300

    Update party.js

--- Module 4/party.js
+++ Module 4/party.js
@@ -19,24 +19,8 @@ do {
     alert ("you are too old");
   }
   // if between 18 and 35, ask for the user name, and welcome the user by his/her name.
-  else if ( age >minAge && age < maxAge){
+  else {
     let name = prompt( "what is your name")
     alert( "welcome "+ name + " to the party");
   }
-} while (confirm("Is there anyone else?")){
-    let age = prompt("Please enter your age");
-   // if current person is younger than 18, will print (you are too young)
-  if ( age < minAge){
-    alert( "you are too young");
-  }
-  // if older than 35, will print (you are too old)
-  else if ( age > maxAge){
-    alert ("you are too old");
-  }
-  // if between 18 and 35, ask for the user name, and welcome the user by his/her name.
-  else if ( age >minAge && age < maxAge){
-    let name = prompt( "what is your name")
-    alert( "welcome "+ name + " to the party");
-  }
-
-}
+} while (confirm("Is there anyone else?"));
